4a3ef755e24abdb73c17d724b26832862aa749f5
SiriCamee <30780788+SiriCamee@users.noreply.github.com>
2021-03-30 11:48:35 +0200

    added Siri's favorite book
    added book that is work related

--- favorite_book.py
+++ favorite_book.py
@@ -1,6 +1,7 @@
 # add your favorite book (or show):
 
 print("Tareq's favorite: When breath becomes air - Paul Kalanithi")
+print("Siri's favorite: Understanding Molecular Simulation - Frenkel and Smit")
 
 print("What is your favorite movie?")
 
